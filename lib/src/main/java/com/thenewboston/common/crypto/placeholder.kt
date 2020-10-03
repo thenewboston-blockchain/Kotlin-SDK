b4db7023f310b90d6369e217f168116c36ddd537
Martin Nowosad <mrtnnwsd@gmail.com>
2020-10-03 21:43:29 +0200

    added placeholder files to not have empty packages

---
+++ lib/src/main/java/com/thenewboston/common/crypto/placeholder.kt
@@ -0,0 +1,6 @@
+package com.thenewboston.common.crypto
+
+
+// this interface will get removed later, it's purpose is to have no packages commmited :)
+interface placeholder {
+}
---
+++ lib/src/main/java/com/thenewboston/common/http/placeholder.kt
@@ -0,0 +1,6 @@
+package com.thenewboston.common.http
+
+
+// this interface will get removed later, it's purpose is to have no packages commmited :)
+interface placeholder {
+}
