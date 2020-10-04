555a8bdb6fb29a5333a858931cee93e7e06437c0
Ian Arbuckle <ian.arbuckle18@gmail.com>
2020-10-04 15:11:16 +0100

    - Add Serialization for json properties * Based on the docs of the API endpoints some properties that are using camel case will need to be serialized https://thenewboston.com/bank-api/banks * Added dependency for Kotlinx serialization library

--- lib/src/main/java/com/thenewboston/bank/model/BankAccount.kt
+++ lib/src/main/java/com/thenewboston/bank/model/BankAccount.kt
@@ -1,9 +1,11 @@
 package com.thenewboston.bank.model
 
+import kotlinx.serialization.SerialName
 import java.util.Date
 
 data class BankAccount(
     val id: String,
+    @SerialName("account_number")
     val accountNumber: String,
     val trust: Float,
     val created: Date,
