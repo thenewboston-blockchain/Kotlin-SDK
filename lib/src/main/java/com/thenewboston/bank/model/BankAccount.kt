eb6e46d79e4d6599062ee60d2779432c27a690b0
freedom <freedom.chuks7@gmail.com>
2020-10-06 09:27:06 -1100

    fixed merge conflict

--- lib/src/main/java/com/thenewboston/bank/model/BankAccount.kt
+++ lib/src/main/java/com/thenewboston/bank/model/BankAccount.kt
@@ -1,10 +1,6 @@
 package com.thenewboston.bank.model
 
-<<<<<<< HEAD
-import kotlinx.datetime.LocalDateTime
-=======
 import java.util.Date
->>>>>>> 6b0b8d4d041131c88b65fd352d54d26096a20f97
 import kotlinx.serialization.SerialName
 
 data class BankAccount(
@@ -12,13 +8,7 @@ data class BankAccount(
     @SerialName("account_number")
     val accountNumber: String,
     val trust: Float,
-<<<<<<< HEAD
-    val created: LocalDateTime,
-    val modified: LocalDateTime
-)
-
-=======
     val created: Date,
     val modified: Date
 )
->>>>>>> 6b0b8d4d041131c88b65fd352d54d26096a20f97
+
