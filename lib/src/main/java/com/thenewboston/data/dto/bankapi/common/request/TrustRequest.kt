8075e5445bff95c90095e810aa525a0b6bf27f70
tomijaga <jagatomi12@gmail.com>
2020-12-31 19:53:43 -0500

    --

--- lib/src/main/java/com/thenewboston/data/dto/bankapi/common/request/TrustRequest.kt
+++ lib/src/main/java/com/thenewboston/data/dto/bankapi/common/request/TrustRequest.kt
@@ -6,8 +6,17 @@ import com.thenewboston.data.dto.bankapi.common.request.PostRequest
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+data class UpdateTrustRequest (
 
-typealias UpdateTrustRequest = @Serializable PostRequest<TrustMessage>
+    @SerialName("message")
+    val message: TrustMessage,
+
+    @SerialName("node_identifier")
+    val nodeIdentifier: String,
+
+    @SerialName("signature")
+    val signature: String
+)
 
 @Serializable
 data class TrustMessage(
--- lib/src/main/java/com/thenewboston/data/dto/bankapi/invalidblockdto/request/PostInvalidBlockRequest.kt
+++ lib/src/main/java/com/thenewboston/data/dto/bankapi/invalidblockdto/request/PostInvalidBlockRequest.kt
@@ -6,9 +6,18 @@ import com.thenewboston.data.dto.bankapi.common.request.PostRequest
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+@Serializable
+data class PostInvalidBlockRequest (
 
+    @SerialName("message")
+    val message: InvalidBlockMessage,
 
-typealias PostInvalidBlockRequest = PostRequest<InvalidBlockMessage>
+    @SerialName("node_identifier")
+    val nodeIdentifier: String,
+
+    @SerialName("signature")
+    val signature: String
+)
 
 @Serializable
 data class InvalidBlockMessage(
